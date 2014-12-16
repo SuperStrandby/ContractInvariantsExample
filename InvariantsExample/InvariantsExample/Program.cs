68fac0d9bbe6a5a7f10d9d0ffcf3689ca1b1b339
SuperStrandby <strandby@live.dk>
2014-12-16 12:32:15 +0100

    done

---
+++ InvariantsExample/InvariantsExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvariantsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Johne", 10);
+            
+            Console.WriteLine("Name: {0}, Age: {1}", student.Name, student.Age);
+        }
+    }
+}
---
+++ InvariantsExample/InvariantsExample/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvariantsExample
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Student(string name, int age)
+        {
+            Contract.Requires<ArgumentException>(name.Length >= 5);
+            Contract.Requires<ArgumentException>(age >= 10);
+            Name = name;
+            Age = age;
+        }
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(Name.Length >= 5);
+            Contract.Invariant(Age >= 10);
+        }
+    }
+}
